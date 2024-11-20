3328c2eef991b46484778ac72edf5c3cb6acecdb
Michael MACHADO <michael.machado.reino@gmail.com>
2024-11-20 21:59:10 +0100

    Update scaleway provider version

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "2.27.0"
+      version = "2.47.0"
     }
   }
   required_version = ">= 0.13"
